57385ba02eb83bd1f072b24dd51d514e57399909
Nathan LOWE <nlowe@e3r7p19.42.fr>
2017-01-12 17:55:32 +0100

    validation changes

--- file_validation.c
+++ file_validation.c
@@ -6,7 +6,7 @@
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/28 23:02:21 by nlowe             #+#    #+#             */
-/*   Updated: 2017/01/10 18:45:44 by nlowe            ###   ########.fr       */
+/*   Updated: 2017/01/12 17:55:15 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,42 +28,78 @@ char	*read_file(char *filename)
 	return (contents);
 }
 
+int		valid_place(char *str)
+{
+	if (!(*str) || *str != '#')
+		return (0);
+	if (*(str + 1) && *(str + 1) == '#')
+		return (1);
+	else if (*(str - 1) && *(str - 1) == '#')
+		return (1);
+	else if (*(str + 5) && *(str + 5) == '#')
+		return (1);
+	else if (*(str - 5) && *(str - 5) == '#')
+		return (1);
+	return (0);
+}
+
+int		test_character(char *str)
+{
+	if (*str == '#' && valid_place(str))
+		return (1);
+	else if (*str == '.')
+		return (-1);
+	else
+		ft_exit("The file provided is not valid.\nError: char test fail");
+	return (0);
+}
+
+int		test_eol(char *str, int *count, int *lines)
+{
+	printf("**str**\n%s\ncount = %i\nlines = %i\n", str, *count, *lines);
+	if (*lines > 3)
+		ft_exit("The file provided is not valid.\nError: number of lines fail");
+	if (!(*(str + 1)) || *(str + 1) == '\n')
+	{
+		if (*count == -8 && *lines == 3)
+		{
+			printf("double eol\n");
+			*lines = 0;
+			*count = 0;
+			str = str + 1;
+			return (1);
+		}
+		else
+			ft_exit("The file provided is not valid.\nError: eol test fail");
+	}
+	else
+	{
+		printf("normal eol\n");
+		(*lines)++;
+	}
+	return (0);
+}
+
 int		num_of_pieces(char *str)
 {
 	int	lines;
-	int	pounds;
 	int	pieces;
-	int	other;
+	int	count;
 
 	lines = 0;
-	pounds = 0;
 	pieces = 0;
-	other = 0;
+	count = 0;
 
 	while (*str)
 	{
-		if (*str == '#')
-			pounds++;
-		else if (*str == '\n')
-			lines++;
-		else if (*str == '.')
-			other++;
+		if (*str == '\n')
+			pieces = pieces + test_eol(str, &count, &lines);
 		else
-			ft_exit("The file provided is not valid.");
-		printf("lines = %i\npounds = %i\npieces = %i\nother = %i\n---\n", lines, pounds, pieces, other);
-		if ((*(str + 1) == '\0' && lines == 4) || lines == 5)
-		{
-			if (pounds != 4 || other != 12)
-				ft_exit("The file provided is not valid.");
-			pieces++;
-			lines = 0;
-			pounds = 0;
-			other = 0;
-		}
+			count = count + test_character(str);
 		str++;
 	}
 	if (pieces == 0 || pieces > 26)
-		ft_exit("The file provided is not valid.");
+		ft_exit("The file provided is not valid.\nError: num pieces test fail");
 	return (pieces);
 }
 /*
